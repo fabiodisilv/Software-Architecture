b3c30f54b523965d9fb50785e299f65034d647ab
tiziasan <tizianosantilli@gmail.com>
2019-01-29 19:29:50 +0100

    giacomo

--- processor/src/main/java/model/Deleted.java
+++ processor/src/main/java/model/Deleted.java
@@ -7,9 +7,18 @@ public class Deleted {
 	private String hold_type = "ProcessEquipHold";
 	private String hold_flag;
 	private String event_datetime;
-
+	
 	public Deleted() {
 		
+	}
+
+	public Deleted(String equip_OID, String recipe_OID, String step_OID, String hold_flag, String event_datetime) {
+		super();
+		this.equip_OID = equip_OID;
+		this.recipe_OID = recipe_OID;
+		this.step_OID = step_OID;
+		this.hold_flag = hold_flag;
+		this.event_datetime = event_datetime;
 	}
 
 	public String getEquip_OID() {
